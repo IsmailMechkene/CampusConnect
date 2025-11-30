f6b0994084c401159385e1f56fd26b3025c6ea8f
Ismail Mechkene <imechkene@gmail.com>
2025-11-30 12:58:26 +0100

    feat(marketplace): enhance trending section UI and update product component
    - Improve trending-section layout and interactions (HTML + TS)
    - Refine styles for featured shops and product component (CSS)
    - Add product images to public assets

--- frontend/src/app/features/marketplace/components/trending-section/trending-section.ts
+++ frontend/src/app/features/marketplace/components/trending-section/trending-section.ts
@@ -13,9 +13,9 @@ import { ProductComponent } from '../../../../shared/components/product-componen
 })
 export class TrendingSection {
   trendingProducts = [
-    { id: 1, name: 'Product A', brand: 'Brand A', price: 29.99, currency: 'USD', rating: 4.5, imageUrl: 'url_to_image_A', isFavorite: false },
-    { id: 2, name: 'Product B', brand: 'Brand B', price: 49.99, currency: 'USD', rating: 4.0, imageUrl: 'url_to_image_B', isFavorite: true },
-    { id: 3, name: 'Product C', brand: 'Brand C', price: 19.99, currency: 'USD', rating: 3.5, imageUrl: 'url_to_image_C' },
-    { id: 4, name: 'Product D', brand: 'Brand D', price: 99.99, currency: 'USD', rating: 5.0, imageUrl: 'url_to_image_D', isFavorite: true },
+    { id: 1, name: 'Stickers', brand: 'StickNation', price: 29.99, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product1.jpg', isFavorite: false },
+    { id: 2, name: 'bracelets', brand: 'bracelet.hand.made', price: 49.99, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product2.png', isFavorite: true },
+    { id: 3, name: 'bracelets', brand: 'SilVerr', price: 19.99, currency: 'TND', rating: 3.5, imageUrl: '/images/products/product3.jpg', isFavorite: false },
+    { id: 4, name: 'Notebooks', brand: 'El Waraq', price: 99.99, currency: 'TND', rating: 5.0, imageUrl: '/images/products/product4.jpg', isFavorite: true },
   ];
 }
