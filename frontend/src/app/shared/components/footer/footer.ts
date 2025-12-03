9f0f920cfa4a7dda767b18d5019683f3384077f7
Ismail Mechkene <imechkene@gmail.com>
2025-12-03 18:49:35 +0100

    feat: added footer

---
+++ frontend/src/app/shared/components/footer/footer.ts
@@ -0,0 +1,35 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    CommonModule,
+    NgFor
+  ],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+  currentYear = new Date().getFullYear();
+
+  links = [
+    { label: 'Home', url: '#' },
+    { label: 'Marketplace', url: '#' },
+    { label: 'About', url: '#' },
+    { label: 'Contact', url: '#' }
+  ];
+
+  socials = [
+    { label: 'Facebook', url: '#' },
+    { label: 'Instagram', url: '#' },
+    { label: 'LinkedIn', url: '#' },
+    { label: 'Twitter', url: '#' }
+  ];
+
+  legal = [
+    { label: 'Terms of service', url: '#' },
+    { label: 'Privacy policy', url: '#' },
+    { label: 'Cookie policy', url: '#' }
+  ];
+}
