b5d9b3bbeee735d19f7b9216902a929ba58871d6
Melek <melek.temimi@gmail.com>
2025-12-10 23:29:53 +0100

    fix: fixed marketplace ui & linked brands

--- frontend/src/app/features/marketplace/components/featured-shops/featured-shops.ts
+++ frontend/src/app/features/marketplace/components/featured-shops/featured-shops.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ShopComponent } from "../../../../shared/components/shop-component/shop-component";
 import { CommonModule } from '@angular/common';
-
+import { Input } from '@angular/core';
 @Component({
     selector: 'app-featured-shops',
     imports: [
@@ -14,15 +14,18 @@ import { CommonModule } from '@angular/common';
 export class FeaturedShops {
     colors = [
         '#FADED2',
-        '#A1B8E9', 
-        '#D4627F', 
-        '#D2A899', 
-        '#DC97A5', 
-        '#EFC9B9' 
+        '#A1B8E9',
+        '#D4627F',
+        '#D2A899',
+        '#DC97A5',
+        '#EFC9B9'
     ];
+    @Input() shop!: { name: string, background: string };
+    @Input() background!: string;
+
 
     shops = [
-        { name: 'Crafty Creations', background: this.colors[0] },
+        { name: 'SilVerr', background: this.colors[0] },
         { name: 'Urban Styles', background: this.colors[1] },
         { name: 'Vintage Vibes', background: this.colors[2] },
         { name: 'Tech Trends', background: this.colors[3] },
--- frontend/src/app/features/marketplace/components/trending-section/trending-section.ts
+++ frontend/src/app/features/marketplace/components/trending-section/trending-section.ts
@@ -13,9 +13,12 @@ import { ProductComponent } from '../../../../shared/components/product-componen
 })
 export class TrendingSection {
   trendingProducts = [
-    { id: 1, name: 'Stickers', brand: 'StickNation', price: 29.99, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product1.jpg', isFavorite: false },
-    { id: 2, name: 'bracelets', brand: 'bracelet.hand.made', price: 49.99, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product2.png', isFavorite: true },
-    { id: 3, name: 'bracelets', brand: 'SilVerr', price: 19.99, currency: 'TND', rating: 3.5, imageUrl: '/images/products/product3.jpg', isFavorite: false },
-    { id: 4, name: 'Notebooks', brand: 'El Waraq', price: 99.99, currency: 'TND', rating: 5.0, imageUrl: '/images/products/product4.jpg', isFavorite: true },
-  ];
+    { id: 1, name: 'Poster', brand: 'StickNation', price: 15, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product14.jpg', isFavorite: false },
+    { id: 2, name: 'CatHat', brand: 'CrochetwithSima', price: 20, currency: 'TND', rating: 3.5, imageUrl: '/images/products/product6.jpg', isFavorite: false },
+    { id: 3, name: 'Earrings', brand: 'SilVerr', price: 19.99, currency: 'TND', rating: 2.5, imageUrl: '/images/products/product8.jpg', isFavorite: true },
+    { id: 4, name: 'StudyBooks', brand: 'El Waraq', price: 99.99, currency: 'TND', rating: 5.0, imageUrl: '/images/products/product10.jpg', isFavorite: true },
+    { id: 5, name: 'Sunflower Keychain', brand: 'CrochetwithSima', price: 10, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product7.jpg', isFavorite: false },
+    { id: 6, name: 'Car Keychain', brand: 'SheinFinds', price: 24.99, currency: 'TND', rating: 4.5, imageUrl: '/images/products/product9.jpg', isFavorite: true },
+    { id: 7, name: 'ChessPattern', brand: 'Totey', price: 19.99, currency: 'TND', rating: 3.5, imageUrl: '/images/products/product11.jpg', isFavorite: false },
+    { id: 8, name: 'Spiderman Shirt', brand: 'PrintX', price: 99.99, currency: 'TND', rating: 5.0, imageUrl: '/images/products/product13.jpg', isFavorite: false } ];
 }
